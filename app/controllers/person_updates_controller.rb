3d25a7ab75908163d84c2baf8178d3e6e60f3dbd
simonf <simon@home.simonf.net>
2014-08-04 20:35:57 +0100

    working on updates

--- app/controllers/person_updates_controller.rb
+++ app/controllers/person_updates_controller.rb
@@ -1,14 +1,15 @@
 class PersonUpdatesController < ApplicationController
-	before_action :set_person, only: [:index, :show, :edit, :update, :destroy]
+  before_action :set_person, only: [:index, :show, :edit, :update, :destroy]
 
-	def index
-		@updates = @person.weekly_updates_by_project(Date.today-7)
+  def index
+#    @updates = @person.weekly_updates_by_project(Date.today-7)
+    @updates = @person.weekly_updates_by_project(:all)
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_person
+      @person = Person.find(params[:id])
     end
-
-	private
-	# Use callbacks to share common setup or constraints between actions.
-	def set_person
-		@person = Person.find(params[:id])
-	end
 
 end
